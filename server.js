1dcc7b6f69792eeec9f7b8e64902b8d0d760d66f
Michal <michal.zelazny@xpo.com>
2018-04-17 15:30:47 -0700

    save

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+var http = require('http');
+//create a server object:
+
+http.createServer(function (req, res) {
+    res.write('Hello World AGAIN!'); //write a response to the client
+    res.end(); //end the response
+    console.dir(res);
+}).listen(8080); //the server object listens on port 8080
+
+console.log('WORKS');
