1316d08af51b5021e0a63389e5857650ad4a70ec
Michal <michal.zelazny@xpo.com>
2018-04-25 08:13:21 -0700

    save

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http = require('http');
 //create a server object:
 
 http.createServer(function (req, res) {
-    res.write('Hello World AGAIN!'); //write a response to the client
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url); //write a response to the client
     res.end(); //end the response
     console.log('touched server');
 }).listen(8080); //the server object listens on port 8080
